cba088ca8330c517a8cc95b25206801df632d203
jkhockaday-tech <jkhockaday@gmail.com>
2026-05-01 16:31:28 -0500

    Update fetch.js

--- utils/fetch.js
+++ utils/fetch.js
@@ -1,14 +1,16 @@
-const LEAGUE_ID = 169608;
+const LEAGUE_ID = process.env.ESPN_LEAGUE_ID;
 const BASE_URL = "https://lm-api-reads.fantasy.espn.com/apis/v3/games/flb/seasons";
 
 async function fetchESPNData(season, view) {
-  const url = `${BASE_URL}/${season}/segments/0/leagues/${LEAGUE_ID}?view=${view}`;
+  const year = season || process.env.ESPN_SEASON_YEAR || new Date().getFullYear();
+  const url = `${BASE_URL}/${year}/segments/0/leagues/${LEAGUE_ID}?view=${view}`;
+
   const headers = {};
-  
+
   if (view === "kona_player_info") {
     headers["x-fantasy-filter"] = JSON.stringify({
       players: {
-        limit: 1500,
+        limit: 2500,
         sortDraftRanks: {
           sortPriority: 100,
           sortAsc: true,
@@ -17,20 +19,18 @@ async function fetchESPNData(season, view) {
       }
     });
   }
-  
+
   const response = await fetch(url, { headers });
+
   if (!response.ok) {
     throw new Error(`ESPN API error: ${response.status} ${response.statusText}`);
   }
+
   return response.json();
 }
 
-async function fetchTeamMetadata(season = 2025) {
+async function fetchTeamMetadata(season) {
   return fetchESPNData(season, "mTeam");
 }
 
-module.exports = {
-  fetchESPNData,
-  fetchTeamMetadata
-};
-
+module.exports = { fetchESPNData, fetchTeamMetadata };
